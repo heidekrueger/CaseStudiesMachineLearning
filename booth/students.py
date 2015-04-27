df170ed162ccdaae8d2855dd9fd5d06b6a0730b9
wedgeCountry <roland.halbig@posteo.de>
2015-04-27 13:02:50 +0200

    image scaling working now

--- booth/students.py
+++ booth/students.py
@@ -1,25 +1,31 @@
 
-import plone
 import os,sys
+
 import Image
 
+import glob
+import subprocess
 
-def load_standardised_image( filename, path, width, height ):
+##
+## USAGE
+##
+## e.g.
+## name = "images/Our Photos/2015-04-22 19.01.17.jpg"
+## res = load_standardised_image( name , 10, 10 )
+##
+def load_standardised_image( filename, width, height ):
 	
 	###
 	### Load Image
 	###
 
-	image = Image.open("images/Our Photos/2015-04-22 19.01.17.jpg")
-
-	print image.bits, image.size, image.format
-
+	image = Image.open(filename)
 
 	### 
 	### Scale Image
 	###
 
-	new_image = plone.scale.scale.scaleImage(image, width=width, height=height)
+	resied_image = image.resize((width, height), Image.ANTIALIAS)    # best down-sizing filter
 
-	return new_image
-
+	return resied_image
+	

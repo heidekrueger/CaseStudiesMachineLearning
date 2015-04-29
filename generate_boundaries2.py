4cff30c064569551eb2fcdb233ae4b82be73dd99
wedgeCountry <roland.halbig@posteo.de>
2015-04-29 18:42:14 +0200

    plot

--- generate_boundaries2.py
+++ generate_boundaries2.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n_set = 4  # number of sets / classes
-n_pts = 500  # number of points per class
+n_pts = 200  # number of points per class
 
 means = [[2, -2], [-5, 5], [3, 5], [-4, -4]]
 cov0 = [[1, 0], [0, 1]]
@@ -52,11 +52,8 @@ num_subsamples = int(0.1 * n_pts)
 sampled_indexes = [np.random.randint(0, X.shape[0])
                    for i in range(num_subsamples)]
 
-X = np.asarray([X[i] for i in sampled_indexes])
-Y = np.asarray([Y[i] for i in sampled_indexes])
-
-print X.shape
-print Y.shape
+X_sub = np.asarray([X[i] for i in sampled_indexes])
+Y_sub = np.asarray([Y[i] for i in sampled_indexes])
 
 h = .02  # step size in the mesh
 from sklearn import svm
@@ -64,11 +61,12 @@ from sklearn import svm
 # we create an instance of SVM and fit out data. We do not scale our
 # data since we want to plot the support vectors
 C = 10  # SVM regularization parameter
-svc = svm.SVC(kernel='linear', C=C).fit(X, Y)
-rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, Y)
-poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, Y)
-lin_svc = svm.LinearSVC(C=C).fit(X, Y)
+print("1")
+svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, Y)
+print("2")
 
+svc_sub = svm.SVC(kernel='poly', degree=3, C=C).fit(X_sub, Y_sub)
+print("3")
 # create a mesh to plot in
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
@@ -76,16 +74,16 @@ xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
 
 # title for the plots
-titles = ['SVC with linear kernel',
-          'LinearSVC (linear kernel)',
-          'SVC with RBF kernel',
-          'SVC with polynomial (degree 3) kernel']
+titles = ['Whole data set',
+          'Subsampled data set',
+          'Comparison']
 
-
-for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
+X_plot = [X, X_sub, X]
+Y_plot = [Y, Y_sub, Y]
+for i, clf in enumerate((svc, svc_sub, svc_sub)):
     # Plot the decision boundary. For that, we will assign a color to each
     # point in the mesh [x_min, m_max]x[y_min, y_max].
-    plt.subplot(2, 2, i + 1)
+    plt.subplot(3, 1, i + 1)
     plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
@@ -93,9 +91,9 @@ for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
     # Put the result into a color plot
     Z = Z.reshape(xx.shape)
     plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
-
+    print( plt.cm.Paired 	)
     # Plot also the training points
-    plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.Paired)
+    plt.scatter(X_plot[i][:, 0], X_plot[i][:, 1], c=Y_plot[i], cmap=plt.cm.Paired)
     plt.xlabel('Sepal length')
     plt.ylabel('Sepal width')
     plt.xlim(xx.min(), xx.max())

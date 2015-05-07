062bffb57ed8b9f3df77335a9e7949488bd9402f
Stan <Salsinats@MacBook-Pro-de-Stanislas.local>
2015-05-07 06:17:23 +0200

    modification of change_name to move files to /faces and /uglyfaces from /webcam

--- booth/change_name.py
+++ booth/change_name.py
@@ -31,24 +31,51 @@ def change_name(s):
 
     print "saving name : " + s
 
-    date = '2015-05-06'
-    path = "faces/"
+    # date of the photo
+    date = '2015-05-07'
+
+    # path to the webcam rep
+    path_webcam = "../../random_rep/"
+
+    # path to the faces rep
+    path_faces = "faces/"
+
+    # path to the ugly faces
+    path_uglyfaces = "uglyfaces/"
+
+    # extension
     f_ext = ".jpg"
 
-    prefixed = [fn for fn in os.listdir(path) if fn.startswith(date)]
-    print len(prefixed)
+    # gather all files beginning with the prefix date
+    prefixed = [fn for fn in os.listdir(path_webcam) if fn.startswith(date)]
 
     prefixed.sort()
 
-    i = 1
-    new_s = [s + "_" + str(i) + f_ext for i in range(1, 11)]
-    new_s.sort()
+    for i in range(len(prefixed)):
+        o_s = path_webcam + prefixed[i]
+        print o_s
 
-    for i in range(10):
+        l = prefixed[i]
+        l = l.split("-")
+
+        # if the file is the ugly face
+        if len(l) == 4:
+            l = l[-1]
+            l = l.split(".")
+            l = l[0]
+            n_s = path_uglyfaces + l + f_ext
+            print n_s
+
+        # if the file is a normal face
+        else:
+            n_s = path_faces + s + "_" + str(i) + f_ext
+            print n_s
+
+        # move the file
         try:
-            o_s = path + prefixed[i]
-            n_s = path + new_s[i]     
             os.rename(o_s, n_s)
+            print "file moved with succes"
+
         except:
             print("could not find file")
 

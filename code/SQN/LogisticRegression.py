a94e874957f7915b2163ab9a0f7b1506da6c4b89
Stefan Heidekrüger <s.heidekrueger@tum.de>
2015-06-01 19:44:30 +0200

    LogReg final

--- code/SQN/LogisticRegression.py
+++ code/SQN/LogisticRegression.py
@@ -5,23 +5,14 @@ import sklearn as sk
 
 class LogisticRegression():
 	"""
-		Returns objective function F, sample loss function f and gradients g
-		with respect to parameters w
-
-		Parameters:
-			X: Sample matrix (nSamples * nFeatures)
-			y: label vector
+		Class representing LogisticRegression
 	"""
 
 	def __init__(self):
+		# sigmoid function
 		self.sigmoid = lambda z: 1/(1.0+np.exp(-z))
+		# hypothesis function
 		self.h = lambda w, X: self.sigmoid(np.dot(X,w))
-
-	#define sigmoid function
-	
-
-	#define hypothesis function for ith sample
-	
 
 	def f(self, w, X, y):
 		"""
@@ -31,7 +22,6 @@ class LogisticRegression():
 			y = np.array([y])
 		hyp = self.h(w, X)
 		return -y*np.log(hyp)- (1-y)*(np.log(1-hyp))
-
 
 	def F(self, w, X, y):
 		"""
@@ -53,14 +43,7 @@ class LogisticRegressionTest(LogisticRegression):
 	def testF(self):
 		iris = datasets.load_iris()
 		X, y = iris.data[:5,:], iris.target[:5,np.newaxis]
-
-		clf = sklearn.linear_model.LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, fit_intercept=True, intercept_scaling=1, class_weight=None, random_state=None, solver='liblinear', max_iter=100, multi_class='ovr', verbose=0)
-
 		w= np.zeros([X.shape[1],1])
-
-		print X
-
-		print y
 
 		print "f complete"
 		print self.f(w, X, y)
@@ -68,5 +51,5 @@ class LogisticRegressionTest(LogisticRegression):
 		print self.f(w, X[0,:], y[0])
 		print "F"
 		print self.F(w,X,y)
-		print "g complete"
+		print "g "
 		print self.g(w, X, y)

89b2697d2c81717f18d2474f98bb0c179e116da6
Stan <Salsinats@MacBook-Pro-de-Stanislas.local>
2015-06-23 20:01:52 +0200

    Fin and Jakob's modification added in test script

--- code/DictLearning/dictionary_learning_test.py
+++ code/DictLearning/dictionary_learning_test.py
@@ -4,8 +4,11 @@
 
 """
 from dictionary_learning import StochasticDictionaryLearning
+from sklearn.feature_extraction.image import reconstruct_from_patches_2d
+from sklearn.linear_model import OrthogonalMatchingPursuit
 import numpy as np
 from time import time
+import matplotlib.pyplot as plt
 
 
 class StochasticDictionaryLearningTest(StochasticDictionaryLearning):
@@ -58,5 +61,95 @@ def load_data():
     return data
 
 
+def load_data_Fin_Jakob():
+    '''
+    code from sklearn application of dictionary learning
+
+    RETURNS:
+    - data: n_patches, dim_patch array like. collection of patches
+    '''
+    from sklearn.feature_extraction.image import extract_patches_2d
+    from scipy.misc import lena
+
+    # Load Lena image and extract patches
+    lena = lena() / 256.0
+
+    # downsample for higher speed
+    lena = lena[::2, ::2] + lena[1::2, ::2] + lena[::2, 1::2] + lena[1::2, 1::2]
+    lena /= 4.0
+    height, width = lena.shape
+
+    # Distort the right half of the image
+    print('Distorting image...')
+    distorted = lena.copy()
+    distorted[:, width // 2:] += 0.075 * np.random.randn(height, width // 2)
+
+    # Extract all reference patches from the left half of the image
+    print('Extracting reference patches...')
+    t0 = time()
+    patch_size = (7, 7)
+
+    data_left = extract_patches_2d(distorted[:, :width // 2], patch_size)
+    data_left = data_left.reshape(data_left.shape[0], -1)
+    data_left -= np.mean(data_left, axis=0)
+    data_left /= np.std(data_left, axis=0)
+
+    data_right = extract_patches_2d(distorted[:, width // 2:], patch_size)
+    data_right = data_right.reshape(data_right.shape[0], -1)
+    center = np.mean(data_right, axis=0)
+    data_right -= center
+    print('done in %.2fs.' % (time() - t0))
+
+    return distorted, data_left, data_right, center
+
+
+def reconstruct_data(D, data, dist, center):
+    """
+    reconstruct right side from learned dictionary
+
+    INPUTS:
+    - D : learned dictionary
+    - data: patches from right side
+    - dist: array of full distorted image
+    - center: mean of data to reverse centralization
+
+    OUTPUTS:
+    - recon: array of reconstructed image
+    """
+
+    t0 = time()
+    recon = dist.copy()
+    patch_size = (7, 7)
+    lars = OrthogonalMatchingPursuit(n_nonzero_coefs=2)
+    lars.fit(D, data.T)
+    code = lars.coef_
+    patches = np.dot(code, D.T)
+    patches += center
+    patches = patches.reshape(len(data), *patch_size)
+    recon[:, dist.shape[1] // 2:] = reconstruct_from_patches_2d(patches, (dist.shape[0], dist.shape[1] // 2))
+    print('done in %.2fs.' % (time() - t0))
+
+    return recon
+
+
 if __name__ == '__main__':
-    X = load_data()
+    sdl = StochasticDictionaryLearning()
+
+    # Loading data
+    distorted, data_left, data_right, center = load_data_Fin_Jakob()
+
+    # Learning dictionary
+    print("Fitting dictionary...")
+    D = sdl.fit(data_left)
+    print("Dictionary fitted.")
+
+    # Reconstructing/Denoising image
+    print("Reconstructing image...")
+    recon = reconstruct_data(D, data_right, distorted, center)
+    print("Image reconstructed.")
+
+    plt.gray()
+    plt.imshow(distorted)
+    plt.show()
+    plt.imshow(recon)
+    plt.show

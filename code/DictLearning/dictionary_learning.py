20327d42af1fae5c7f48d7591cfc55f0bbf4257f
Slasnista <stan.chambon@gmail.com>
2015-06-27 13:57:05 +0200

    I may have the solution ! ... But I am not sure, still working on it...

--- code/DictLearning/dictionary_learning.py
+++ code/DictLearning/dictionary_learning.py
@@ -52,7 +52,8 @@ class StochasticDictionaryLearning:
     '''
 
     def __init__(self, n_components=100, option=None, alpha=0.001,
-                 n_iter=10, max_iter=100, batch_size=3, verbose=0):
+                 n_iter=10, max_iter=100, epsilon=0.0001, batch_size=200,
+                 verbose=0):
 
         self.n_components = n_components
         self.alpha = alpha
@@ -60,6 +61,7 @@ class StochasticDictionaryLearning:
         self.max_iter = max_iter
         self.batch_size = batch_size
         self.components = []
+        self.epsilon = epsilon
 
         self.option = option
         self.verbose = verbose
@@ -170,43 +172,59 @@ class StochasticDictionaryLearning:
         Q? : should I update D or self.components ??
         '''
 
-        # Nullity control
-        null_control = False
-        c_control = 0
-        max_control = 10
-        z_control = 0.0
+        # # Nullity control
+        # null_control = False
+        # c_control = 0
+        # max_control = 10
+        # z_control = 0.0
 
-        if t == 1:
-            while null_control is False and c_control < max_control:
+        # if t == 1:
+        #     while null_control is False and c_control < max_control:
 
-                # update counter
-                c_control += 1
+        #         # update counter
+        #         c_control += 1
 
-                # 4: Sparse coding with LARS
-                coef = self.lasso_subproblem(Xt, D, A, B, t)
+        #         # 4: Sparse coding with LARS
+        #         coef = self.lasso_subproblem(Xt, D, A, B, t)
 
-                # 5/6: Update A and B
-                A, B = self.update_matrices(Xt, D, coef, A, B, t)
+        #         # 5/6: Update A and B
+        #         A, B = self.update_matrices(Xt, D, coef, A, B, t)
 
-                # Non nullity control
-                z_control = np.sum(A)
+        #         # Non nullity control
+        #         z_control = np.linalg.det(A)
+        #         print z_control
 
-                if z_control != 0:
-                    null_control = True
+        #         if z_control != 0:
+        #             null_control = True
 
-                # if null_control is False and c_control == max_control:
-                #     print "Please use a small value for regularization"
-                #     print "paremeter alpha"
+        #         # if null_control is False and c_control == max_control:
+        #         #     print "Please use a small value for regularization"
+        #         #     print "paremeter alpha"
 
-        else:
-            null_control = True
-            # 4: Sparse coding with LARS
-            coef = self.lasso_subproblem(Xt, D, A, B, t)
+        # else:
+        #     null_control = True
+        #     # 4: Sparse coding with LARS
+        #     coef = self.lasso_subproblem(Xt, D, A, B, t)
 
-            # 5/6: Update A and B
-            A, B = self.update_matrices(Xt, D, coef, A, B, t)
+        #     # 5/6: Update A and B
+        #     A, B = self.update_matrices(Xt, D, coef, A, B, t)
 
-        return null_control
+        # if null_control is True:
+        #     D = self.dictionary_update(D, A, B)
+
+        # return null_control
+
+        # 4: Sparse coding with LARS
+        coef = self.lasso_subproblem(Xt, D, A, B, t)
+
+        # 5/6: Update A and B
+        A, B = self.update_matrices(Xt, D, coef, A, B, t)
+        z_control = np.linalg.det(A)
+        print z_control
+
+        D = self.dictionary_update(D, A, B)
+
+        return True
 
     def lasso_subproblem(self, Xt, D, A, B, t):
         '''
@@ -283,92 +301,166 @@ class StochasticDictionaryLearning:
 
         return A, B
 
+    def dictionary_update(self, D, A, B):
+        '''
+        Dictionary update
 
-def algorithm1(x, n_components=100,
-               alpha=0.01, n_iter=30, batch_size=3, verbose=0):
-    '''
-    Online dictionary learning algorithm
+        INPUTS:
+        - self
+        - D, dictionary
+        - A, matrix
+        - B, matrix
 
-    INPUTS:
-    - x : (n_samples, m) array like, data
-    - l, regularization parameter
-    - n_components, number of components of dictionary
-    - n_iter, int, number of iterations
-    - batch_size, int, mini batch size
-    - verbose, int, control verbosity of algorithm
 
-    OUTPUTS:
-    - D : (m, k) array like, learned dictionary
-    '''
+        OUTPUT:
+        - D, updated dictionary
+        '''
 
-    n_s = len(x[:, 0])  # number of samples in x
+        [m, k] = D.shape
 
-    D = np.random.rand(len(x[0, :]), n_components)  # initial dictionary
-    print D.shape
+        c = 0  # counter
+        cv = False  # convergence or stop indicator
 
-    # Dimensions of dicitonary
-    [m, k] = D.shape
+        # 2: loop to update each column
+        while cv is not True:
 
-    # 1: initialization
-    A = np.zeros((k, k))
-    B = np.zeros((m, k))
+            # keep a trace of previous dictionary
+            D_old = np.zeros((m, k))
+            for i in range(0, m):
+                for j in range(0, k):
+                    D_old[i, j] = D[i, j]
 
-    '''
-    First iteration in a while loop so that matrix A in not empty
-    - if empty => change l
-    - if empty => change batch size
+            for j in range(0, k):
 
-    So that we ensure that no problem occurs in algo2
-    '''
+                # 3: Update the j-th column of d
+                s_A = np.sum(A[:, j])
+                s_B = np.sum(B[:, j])
+                a_jj = A[j, j]
 
-    '''
-    Loop
-    for t in range(2, n_iter + 1):
+                print "s_A", s_A
+                print "s_B", s_B
+                print "a_jj", a_jj
+                print ""
 
-    '''
+                if s_A + s_B == 0 and a_jj == 0:
+                    u = 1 + np.asmatrix(D[:, j]).T
+                else:
+                    u = (1 / A[j, j]) * (B[:, j] - D.dot(A[:, j]))
+                    u = u + np.asmatrix(D[:, j]).T
 
-    # 2: Loop
-    for t in range(1, n_iter + 1):
+                # renormalisation
+                renorm = max(np.linalg.norm(u), 1)
+                u = np.divide(u, renorm)
 
-        # 3: Draw xj from x
-        j = np.random.randint(0, n_s, batch_size)
-        xt = x[j, :]
-        xt = np.asmatrix(xt).T
+                for p in range(0, m):
+                    D[p, j] = u[p]
 
-        # 4: Sparse coding with LARS
-        from sklearn.linear_model import LassoLars
-        lars = LassoLars(alpha=alpha)
-        # Lars LassoLars
+            # counter update
+            c += 1
 
-        lars.fit(D, xt)
-        coeff = lars.coef_
-        coeff = (np.asmatrix(coeff)).T
+            # compute differences between two updates
+            grad = D - D_old
+            crit = np.linalg.norm(grad)
 
-        # computing coefficient beta for step 5/6
-        if t < batch_size:
-            theta = float(t * batch_size)
-        else:
-            theta = math.pow(batch_size, 2) + t - batch_size
+            # check convergence
+            if crit < self.epsilon:
+                cv = True
+            if c > self.max_iter:
+                cv = True
 
-        beta = (theta + 1 - batch_size) / (theta + 1)
+        if c == self.max_iter and self.verbose > 10:
+            print "Dictionary Updating Algo reached max number of iterations"
+            print "Consider higher max number of interations"
 
-        # 5: Update A
-        a = np.zeros((k, k))
-        for i in range(0, batch_size):
-            a = a + (coeff[:, i]).dot(coeff[:, i].T)
-        A = beta * A + a
+        # 6: Return updated dictionary
+        return D
 
-        # 6: Update B
-        b = np.zeros((m, k))
-        for i in range(0, batch_size):
-            b = b + xt[:, i].dot(coeff[:, i].T)
-        B = beta * B + b
 
-        # Compute new dictionary update
-        # D = algorithm2(D, A, B)
+# def algorithm1(x, n_components=100, alpha=0.01, n_iter=30, batch_size=3,
+#                verbose=0):
+#     '''
+#     Online dictionary learning algorithm
 
-    # 9 : Return learned dictionary
-    return D
+#     INPUTS:
+#     - x : (n_samples, m) array like, data
+#     - l, regularization parameter
+#     - n_components, number of components of dictionary
+#     - n_iter, int, number of iterations
+#     - batch_size, int, mini batch size
+#     - verbose, int, control verbosity of algorithm
+
+#     OUTPUTS:
+#     - D : (m, k) array like, learned dictionary
+#     '''
+
+#     n_s = len(x[:, 0])  # number of samples in x
+
+#     D = np.random.rand(len(x[0, :]), n_components)  # initial dictionary
+#     print D.shape
+
+#     # Dimensions of dicitonary
+#     [m, k] = D.shape
+
+#     # 1: initialization
+#     A = np.zeros((k, k))
+#     B = np.zeros((m, k))
+
+#     '''
+#     First iteration in a while loop so that matrix A in not empty
+#     - if empty => change l
+#     - if empty => change batch size
+
+#     So that we ensure that no problem occurs in algo2
+#     '''
+
+#     '''
+#     Loop
+#     for t in range(2, n_iter + 1):
+
+#     '''
+
+#     # 2: Loop
+#     for t in range(1, n_iter + 1):
+
+#         # 3: Draw xj from x
+#         j = np.random.randint(0, n_s, batch_size)
+#         xt = x[j, :]
+#         xt = np.asmatrix(xt).T
+
+#         # 4: Sparse coding with LARS
+#         from sklearn.linear_model import LassoLars
+#         lars = LassoLars(alpha=alpha)
+#         # Lars LassoLars
+
+#         lars.fit(D, xt)
+#         coeff = lars.coef_
+#         coeff = (np.asmatrix(coeff)).T
+
+#         # computing coefficient beta for step 5/6
+#         if t < batch_size:
+#             theta = float(t * batch_size)
+#         else:
+#             theta = math.pow(batch_size, 2) + t - batch_size
+
+#         beta = (theta + 1 - batch_size) / (theta + 1)
+
+#         # 5: Update A
+#         a = np.zeros((k, k))
+#         for i in range(0, batch_size):
+#             a = a + (coeff[:, i]).dot(coeff[:, i].T)
+#         A = beta * A + a
+
+#         # 6: Update B
+#         b = np.zeros((m, k))
+#         for i in range(0, batch_size):
+#             b = b + xt[:, i].dot(coeff[:, i].T)
+#         B = beta * B + b
+
+#         # Compute new dictionary update
+#         # D = algorithm2(D, A, B)
+
+#     # 9 : Return learned dictionary
+#     return D
 
 
 def algorithm2(D, A, B, c_max=3, eps=0.00001):

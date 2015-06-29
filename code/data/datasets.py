d6b15904f69f14e4795632c8b375bc5e9eea2625
Slasnista <stan.chambon@gmail.com>
2015-06-29 22:15:25 +0200

    creation of a function to load eeg data that could be added to datasets.py

--- code/data/datasets.py
+++ code/data/datasets.py
@@ -6,7 +6,8 @@ try:
     import MySQLdb
 except:
     import pymysql as MySQLdb
-#### functions for reading from loooong file as stream
+# functions for reading from loooong file as stream
+
 
 def getstuff(filename, rowlim):
     with open(filename, "rb") as csvfile:
@@ -19,48 +20,56 @@ def getstuff(filename, rowlim):
             else:
                 return
 
+
 def getdata(filename, rowlim):
     for row in getstuff(filename, rowlim):
         yield row
 
 #####
 
+
 def normalize(X):
     mu = X.mean(axis=0)
     std = X.std(axis=0)
     for i in range(X.shape[0]):
-        X[i,:] -= mu
+        X[i, :] -= mu
     for j in range(X.shape[1]):
-        X[i,j] /= std[j]
+        X[i, j] /= std[j]
     return X
 
+
 def load_data1():
-    dataset = np.genfromtxt(open('../datasets/ex2data1.txt','r'), delimiter=',', dtype='f8')
-    X = dataset[:,:2]
-    z = dataset[:,2][:,np.newaxis] 
-    
+    dataset = np.genfromtxt(open('../datasets/ex2data1.txt', 'r'),
+                            delimiter=',', dtype='f8')
+    X = dataset[:, :2]
+    z = dataset[:, 2][:, np.newaxis]
+
     X = normalize(X)
-    
-    X = np.concatenate( (np.ones( (len(z), 1)), X), axis=1)
+
+    X = np.concatenate((np.ones((len(z), 1)), X), axis=1)
     X_new = []
     for i in range(len(z)):
-        x = np.array(list(X[i,:].flatten()))
+        x = np.array(list(X[i, :].flatten()))
         X_new.append(x)
     return X_new, list(z)
 
+
 def load_data2():
-    dataset = np.genfromtxt(open('../datasets/ex2data2.txt','r'), delimiter=',', dtype='f8')
-    X = dataset[:,:2]
-    z = dataset[:,2][:,np.newaxis]
-    
+    dataset = np.genfromtxt(open('../datasets/ex2data2.txt', 'r'),
+                            delimiter=',', dtype='f8')
+
+    X = dataset[:, :2]
+    z = dataset[:, 2][:, np.newaxis]
+
     X = normalize(X)
-    
-    X = np.concatenate( (np.ones( (len(z), 1)), X), axis=1)
+
+    X = np.concatenate((np.ones((len(z), 1)), X), axis=1)
     X_new = []
     for i in range(len(z)):
-        x = np.array(list(X[i,:].flatten()))
+        x = np.array(list(X[i, :].flatten()))
         X_new.append(x)
     return X_new, list(z)
+
 
 def load_iris():
     iris = sklearn.datasets.load_iris()
@@ -86,38 +95,41 @@ def split_into_files(src, dest_folder):
     csvfile.close()
 
 
-
-
 """
-    Before you can use MySQL as database, you first have to create a user 'casestudies' using the admin/root account.
-    Then you have to make sure that the database HIGGS is created and that the user has access to it.
+    Before you can use MySQL as database, you first have to create a user
+    'casestudies' using the admin/root account.
+
+    Then you have to make sure that the database HIGGS is created and that
+    the user has access to it.
+
     mysql -u root -p
     CREATE USER 'casestudies'@'localhost';
     CREATE DATABASE HIGGS;
     GRANT ALL PRIVILEGES ON HIGGS.* TO 'casestudies'@'localhost';
 """
-    
-def get_mysql():     
-    db = MySQLdb.connect(
-                user="casestudies",
-                db="HIGGS"
-                ) # name of the data base    
+
+
+def get_mysql():
+    db = MySQLdb.connect(user="casestudies",
+                         db="HIGGS")  # name of the data base
+
     cur = db.cursor()
-    
+
     table_name = "DATA"
     dimensions = 29
-    
+
     return db, cur, table_name, dimensions
 
 
 def create_higgs():
-    db, cur, table_name, dimensions = get_mysql() 
-    sql =   "CREATE TABLE IF NOT EXISTS " + table_name + " (ID INTEGER PRIMARY KEY, "
+    db, cur, table_name, dimensions = get_mysql()
+    sql = "CREATE TABLE IF NOT EXISTS " + table_name + " (ID INTEGER PRIMARY KEY, "
+
     for i in range(dimensions):
-        sql += "x_" + str(i) + " DOUBLE, " 
+        sql += "x_" + str(i) + " DOUBLE, "
     sql = sql[:-2]
     sql += ");"
-    
+
     try:
         cur.execute(sql)
         # cur.execute("CREATE UNIQUE INDEX id_index ON DATA (ID) USING BTREE;")
@@ -125,31 +137,31 @@ def create_higgs():
         print(w)
     cur.close()
     db.close()
-    
+
 
 def load_higgs_into_mysql():
-    
+
     create_higgs()
-    db, cur, table_name, dimensions = get_mysql() 
-    
+    db, cur, table_name, dimensions = get_mysql()
+
     generic = "INSERT INTO " + table_name + " VALUES ("
-    
+
     file_name = '../../datasets/HIGGS.csv'
     csvfile = open(file_name, "r")
-    
+
     for count, line in enumerate(iter(csvfile)):
-        line = re.sub('\s','',str(line))
-        entries = re.split(",",str(line))
-        insert_statement = generic 
+        line = re.sub('\s', '', str(line))
+        entries = re.split(", ", str(line))
+        insert_statement = generic
         insert_statement += str(count) + ","
         for index, entry in enumerate(entries):
             if index >= dimensions:
                 break
             insert_statement += str(entry) + ","
-            
+
         insert_statement = insert_statement[:-1]
-        insert_statement += ')' 
-        if count%1000==0:
+        insert_statement += ')'
+        if count % 1000 == 0:
             print(count)
         if count > 1e6:
             break
@@ -157,29 +169,30 @@ def load_higgs_into_mysql():
             cur.execute(insert_statement)
         except:
             continue
-    
+
     csvfile.close()
-    
+
     db.commit()
     cur.close()
     db.close()
 
+
 def get_higgs_mysql(ID_list):
-    
-    db, cur, table_name, dimensions = get_mysql() 
-    
+
+    db, cur, table_name, dimensions = get_mysql()
+
     query = "SELECT * FROM " + table_name + " WHERE ID IN ("
     for ID in ID_list:
         query += "'" + str(ID) + "'" + ","
     query = query[:-1]
     query += ");"
-    #print query
+    # print query
     cur.execute(query)
     X, y = [], []
     for c in cur:
         X_tmp, y_tmp = [], None
         for index in range(len(c)):
-            if index == 0: 
+            if index == 0:
                 continue
             elif index == 1:
                 y_tmp = c[index]
@@ -189,24 +202,22 @@ def get_higgs_mysql(ID_list):
         X.append(np.array(X_tmp))
         y.append(y_tmp)
     cur.close()
-    #db.close()
-    
+    # db.close()
+
     return X, y
-    
-    
+
+
 def load_higgs(rowlim=1000):
     file_name = '../datasets/HIGGS.csv'
     X, y = [], []
     for row in getdata(file_name, rowlim):
         X.append(np.array([1.0] + [float(r) for r in row[1:]]))
-        y.append(float(row[0])) 
+        y.append(float(row[0]))
     # print type(X[0])
     # print X[0][0]
     return X, y
 
 
-
 if __name__ == "__main__":
     load_higgs_into_mysql()
-    print(get_higgs_mysql([1,2,5, 55, 332, 3456, 0]))
-    
+    print(get_higgs_mysql([1, 2, 5, 55, 332, 3456, 0]))

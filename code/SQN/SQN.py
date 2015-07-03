Replaced np.dot by np.inner in armijo. Should no longer give wrong dimensions.

code/SQN/SQN.py
@@ -207,7 +207,7 @@ class SQN(StochasticOptimizer):
         #print candidate * gamma * np.dot( g(x).T, s)
         #print s
         #print "---"
-        while (f(x + np.multiply(candidate, s)) - f(x) > candidate * gamma * np.dot( g(x).T, s)) and candidate > 1e-4:
+        while (f(x + np.multiply(candidate, s)) - f(x) > candidate * gamma * np.inner( g(x), s)) and candidate > 1e-4:
         
         #    print "armijo"
         #    print f(x + np.multiply(candidate, s)) - f(x)
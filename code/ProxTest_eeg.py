c0297ba4921a2735c7504900ba870607997903af
Stefan Heidekrüger <s.heidekrueger@tum.de>
2015-07-05 15:27:12 +0200

    fixed minor errors, added debug stuff in proxtest

--- code/ProxTest_eeg.py
+++ code/ProxTest_eeg.py
@@ -6,7 +6,7 @@ Created on Sat Jul  4 17:57:01 2015
 """
 
 import numpy as np
-from data.LogisticRegression import LogisticRegression
+from SQN.LogisticRegression import LogisticRegression
 import data.ProxMeth as pm
 import data.datasets as ds
 
@@ -17,8 +17,8 @@ lr = LogisticRegression()
 print "X, y loaded, initializing F"
 f = lambda x: lr.F(x, X, y)
 print "done. initializing gf"
-gf = lambda w: sum([lr.g(w, x, z) for x,z in zip(X,y)])/len(y)
-
+gf = lambda w: lr.G(w, X, y)
+print "done."
 # x0 finden etc. hofffen dass es läuft
 xstart=np.zeros(600)
 

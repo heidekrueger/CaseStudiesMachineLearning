Moved stochastic gradient to model, so that it now is an argument to the SQN, added convergence criterion using white noise test and variance estimation, corrected errors in sqndict. TODO: Somehow LASSOLARS returns only 0 element recons?

code/SQN/SQN.py
@@ -7,7 +7,7 @@ import numpy as np
 import itertools
 from collections import deque
 import stochastic_tools
-
+from stochastic_tools import test_normality
 """
 TODO: Iterator support not yet tested! Try on Dictionary Learning Problem!
 """
@@ -80,6 +80,8 @@ class StochasticOptimizer:
         self.options['batch_size'] = 1
         self.options['batch_size_H'] = 1
         self.options['max_iter'] = 1e3
+        self.options['testinterval'] = 30
+        self.options['updates_per_batch'] = 1
         # If options is given
         self.set_options(options)
 
@@ -266,6 +268,13 @@ class SQN(StochasticOptimizer):
         else:
             return self.w
 
+    def is_stationary(self):
+            test_len = len(self.f_vals) - self.options['testinterval']
+            return test_len > 0 and test_normality(self.f_vals[test_len:], burn_in=1, level=0.05)     
+    def get_test_variance(self):
+            test_len = len(self.f_vals) - self.options['testinterval']
+            return np.var(self.f_vals[test_len:])
+        
     def solve_one_step(self, f, g, X=None, z=None, k=1):
         """
         perform one update step
@@ -281,9 +290,29 @@ class SQN(StochasticOptimizer):
 
         """
         assert self.w is not None, "Error! weights not initialized!"
+        
+        # Draw sample batch
+        if X is None:
+            X_S, z_S = self._draw_sample(self.options['N'],
+                                         b=self.options['batch_size'])
+        else:
+            X_S, z_S = self._draw_sample(X, z, b=self.options['batch_size'])
+        # Stochastic functions
+        f_S = lambda x: f(x, X_S, z_S)  # if z is not None else f(x, X_S)
+        g_S = lambda x: g(x, X_S, z_S)
 
         # perform gradient update using armijo rule and hessian information
-        self.w = self._perform_update(f, g, X, z)
+        for i in range(self.options['updates_per_batch']):
+                self.w = self._perform_update(f_S, g_S)
+        
+        # Check Termination Condition
+        if len(X_S) == 0 or self._has_terminated(g_S(self.w), self.w):
+            self.termination_counter += 1
+        if k % self.options['testinterval'] == 0 and self.is_stationary() and self.get_test_variance() < 0.005:
+                self.termination_counter += 1
+                if self.debug:
+                    print "stationary"
+        
         if self.debug:
             print(self.w)
 
@@ -358,36 +387,8 @@ class SQN(StochasticOptimizer):
 
         return candidate
 
-    def stochastic_gradient(self, g, w, X=None, z=None):
-        """
-        Calculates Stochastic gradient of F at w as per formula (1.4)
-
-        INPUTS:
-        - self
-        - g
-        - w
-        - X
-        - z
-
-        OUTPUT:
-        """
-        if X is not None:
-            nSamples = len(X)
-            nFeatures = len(X[0])
-
-        if X is None:
-            return g(w)
-
-        elif z is None:
-            return np.array(sum([g(w, X[i]) for i in range(nSamples)]))
-
-        else:
-            assert len(X) == len(z), "Error: Dimensions must match"
-            # print(" one gradient:" , g(w,X[0],z[0]))
-            return sum([g(w, X[i], z[i]) for i in range(nSamples)])
-
     # Calculate gradient and perform update
-    def _perform_update(self, f, g, X, z):
+    def _perform_update(self, f_S, g_S):
         """
         do the gradient updating rule
 
@@ -399,16 +400,6 @@ class SQN(StochasticOptimizer):
         - z
 
         """
-        # Draw sample batch
-        if X is None:
-            X_S, z_S = self._draw_sample(self.options['N'],
-                                         b=self.options['batch_size'])
-        else:
-            X_S, z_S = self._draw_sample(X, z, b=self.options['batch_size'])
-        # Stochastic functions
-        f_S = lambda x: f(x, X_S, z_S)  # if z is not None else f(x, X_S)
-        g_S = lambda x: self.stochastic_gradient(g, x, X_S, z_S)
-
         # Get search direction
         search_direction = self._get_search_direction(g_S)
 
@@ -431,10 +422,6 @@ class SQN(StochasticOptimizer):
         # Store information
         self.f_vals.append(f_S(self.w))
         self.g_norms.append(np.linalg.norm(g_S(self.w)))
-
-        # Check Termination Condition
-        if len(X_S) == 0 or self._has_terminated(g_S(self.w), self.w):
-            self.termination_counter += 1
 
         return self.w
 
@@ -478,7 +465,7 @@ class SQN(StochasticOptimizer):
 
         # draw hessian sample and get the corresponding stochastic gradient
         X_SH, y_SH = self._draw_sample(X, z, b=self.options['batch_size_H'])
-        g_SH = lambda x: self.stochastic_gradient(g, x, X_SH, y_SH)
+        g_SH = lambda x: g(x, X_SH, y_SH)
 
         s_t, y_t = self._get_correction_pairs(g_SH,
                                               self.wbar,
@@ -548,7 +535,7 @@ class SQN(StochasticOptimizer):
             grad: gradient
             w: current variable
         """
-
+        
         if self.debug:
             print("Check termination")
             print("len grad: %f" % np.linalg.norm(grad))

code/sqndict.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 from time import time
 from DictLearning.dictionary_learning import StochasticDictionaryLearning
@@ -9,14 +10,18 @@ class SqnDictionaryLearning(StochasticDictionaryLearning):
         recon = []
 
         def matrix_to_vector(self, D):
-
+                return D.flat[:]
+                """
                 v = []
-                for i in range(D.range[0]):
-                        for j in range(D.range[1]):
+                for i in range(D.shape[0]):
+                        for j in range(D.shape[1]):
                                 v.append(D[i, j])
                 return np.array(v)
+                """
 
         def vector_to_matrix(self, v):
+                return v.reshape((len(v)/self.n_components, self.n_components))
+                """
                 nrow = self.n_components
                 D = np.zeros((len(v)/self.n_components, self.n_components))
 
@@ -29,65 +34,67 @@ class SqnDictionaryLearning(StochasticDictionaryLearning):
                             i += 1
                             j = 0
                 return D
-
+                """
+                
         def f(self, d, X, z=None):
-            #print "shape X:", len(X), len(d), d.shape
+                D = d.reshape(len(d)/self.n_components, self.n_components)
+               # print D
+                one_f = []
+                print np.array(self.recon.T[0].flat)
+                print self.recon.T[0]
+                print "dot", D.dot(np.array(self.recon.T[0].flat))
+                for x, a in zip(X, self.recon.T):
+                        x = np.asarray(x.flat)
+                        a = np.asarray(a.flat)
+                        l2_norm = 0.5 * np.linalg.norm(x - D.dot(a))**2
+                        l1_norm = self.alpha * np.linalg.norm(a, ord=1) 
+                        one_f.append( l2_norm + l1_norm )
+                return sum(one_f) / len(one_f)
 
-            self.recon = []
-
-            if not isinstance(self.recon, list):
-                        self.recon = [self.recon]
-
-            if not isinstance(X, list):
-                        X = [X]
-
-            self.components = self.vector_to_matrix(d)
-
-            #print "doing lasso"
-            if len(self.recon) == 0:
-                    self.recon = [self.lasso_subproblem(np.asmatrix(x).T) for x in X]
-            #print "lasso done"
-
-            one_f = [0.5 * np.linalg.norm(x - (self.components).dot(a))**2 + self.alpha * np.linalg.norm(a, ord=1) for x, a in zip(X, self.recon) ]
-            return sum(one_f) / len(one_f)
-
-        def g(self, d, X, z=None):
-
-                if not isinstance(self.recon, list):
-                        self.recon = [self.recon]
-                if not isinstance(X, list):
-                        X = [X]
-                
-                self.components = self.vector_to_matrix(d)
-
-                if len(self.recon) == 0:
-                        print "doing lasso"
-                        self.recon = [self.lasso_subproblem(np.asmatrix(x).T) for x in X]
-
-                #print "lasso done"
-                x = X[0]
-                a = np.array(self.recon[0].flat)
-                one_g = []
-                #print self.components.shape, x.shape
-                grad = None
-                for x, a in zip(X, self.recon):
+        def finite_differences(self, d, X, z=None):
+                grad = np.zeros(d.shape)
+                r = 0.1
+                for i in range(len(d)):
+                        step = d.copy()
+                        step[i] += r
+                        #print step-d
+                        grad[i] = (self.f(step, X, z) - self.f(d, X, z))/r
+                        print grad[i]
+                return grad
+            
+        def g_fast(self, d, X, z=None):
+                D = d.reshape(len(d)/self.n_components, self.n_components)
+                grad = np.zeros(d.shape)
+                for x, a in zip(X, self.recon.T):
                     a = np.array(a.flat)
-                    for i in range(self.components.shape[1]):
-                        for j in range(self.components.shape[0]):
-                                S = 0.0
-                                for k in range(self.components.shape[0]):
-                        #                print i, k, self.components.shape
-                                        S += self.components[k,i] * x[k]
-                                #print S, x[j], a[i], x[j]
-                                one_g.append(x[j] * S - a[i]*x[j])
-                    
-                    if grad is None:
-                            grad = np.array(one_g)
-                    else:
-                            grad += np.array(one_g)
-                return np.multiply(len(X), grad)
+                    x = np.array(x.flat)
+                    Dx = D.T.dot(x)
+                    grad += (np.outer(Dx, x) - np.outer(a, x)).flat[:]
+            
+                return np.multiply(1.0/len(X), grad) 
+            
+        def g_comp_wise(self, d, X, z=None):
+                D = d.reshape(len(d)/self.n_components, self.n_components)
+                grad = np.zeros(d.shape)
+                for x, a in zip(X, self.recon.T):
+                    a = np.array(a.flat)
+                    x = np.array(x.flat)
+                    index = 0
+                    for i in range(D.shape[1]):
+                            S = 0.0
+                            for k in range(D.shape[0]):
+                                    S += D[k,i] * x[k]
+                            for j in range(D.shape[0]):
+                                    grad[index] += x[j] * S - a[i]*x[j]
+                                    index += 1
+                return np.multiply(1.0/len(X), grad)
                 
-
+        def g(self, d, X, z=None):
+        #        print np.max(self.g_fast(d, X, z) - self.finite_differences(d, X, z))
+                print self.finite_differences(d, X, z)
+                return self.finite_differences(d, X, z)
+                #return self.finite_differences(d, X, z)
+                
         def fit(self, X):
             '''
             This method runs online dictionary learning on data X and update
@@ -102,41 +109,6 @@ class SqnDictionaryLearning(StochasticDictionaryLearning):
             t_fit = time()
             l_tsteps = []
 
-            # number of samples in x
-            n_s = len(X[:, 0])
-
-            # initial dictionary : take some elements of X
-            jd = np.random.randint(0, n_s, self.n_components)
-            D = X[jd, :].T
-            # D = np.random.rand(len(X[0, :]), self.n_components)
-            '''
-            Q? : should I work with self.components ??
-            '''
-            self.components = X[jd, :].T
-
-
-            # Dimensions of dicitonary
-            [m, k] = D.shape
-
-            # 1: initialization
-            # A = np.zeros((k, k))
-            # B = np.zeros((m, k))
-
-            A = np.identity(k)
-            B = X[jd, :].T
-
-            '''
-            Consider rescaling ?
-            '''
-
-            # dimension control
-            if self.verbose > 10:
-                print "Dimensions infos :"
-                print "    n_samples :", n_s
-                print "    D shape :", D.shape
-                print "    A shape :", A.shape
-                print "    B shape :", B.shape
-
             if self.verbose > 0:
                 print "Running online dictionary learning"
                 print str(self.n_iter), "iter to perform"
@@ -147,6 +119,8 @@ class SqnDictionaryLearning(StochasticDictionaryLearning):
                     for i in j:
                             X_S.append(X[i])
                     return X_S, None
+                
+            
             def stochastic_gradient(g, w, X=None, z=None):
                     return g(w, X, z) 
             
@@ -154,23 +128,39 @@ class SqnDictionaryLearning(StochasticDictionaryLearning):
                        'max_iter': self.max_iter,
                        'batch_size': self.batch_size,
                        'beta': 10.,
-                       'M': 10,
+                       'M': 5,
                        'batch_size_H': 10,
-                       'L': 10,
-                       'sampleFunction': sample_batch}
+                       'L': 5,
+                       'updates_per_batch': 30}
             
             sqn = SQN(options)
-         #   sqn.stochastic_gradient = stochastic_gradient
 
-            # sqn.set_options(self.options)
-            #sqn.solve(self.f, self.g, X)
-            "Please provide either a data set or a sampling function"
-
-            sqn.set_start(dim=sqn.options['dim'])
-            import itertools
+            # initial dictionary : take some elements of X
+            jd = np.random.randint(0, len(X[:]), self.n_components)
+            self.components = X[jd, :].T
+            sqn._armijo_rule = lambda f,g,x,s, start, beta, gamma: 0.001
+            sqn.set_start(w1 = np.array(self.components.flat))
+            d = sqn.get_position()
+            print d
             for k in itertools.count():
                 print k
-                w = sqn.solve_one_step(self.f, self.g, X, None, k)
+                j = np.random.randint(0, len(X), sqn.options['batch_size'])
+                Xt = X[j, :]
+                Xt = np.asmatrix(Xt)
+                    
+                def draw_sample(w, X, z, b):
+                    return Xt, None
+                sqn.draw_sample = draw_sample
+                
+                self.components = d.reshape(len(d)/self.n_components, self.n_components)
+                #print self.components
+                self.recon = self.lasso_subproblem(Xt.T)
+                print Xt
+                print self.recon
+                # TODO: WHY???
+                break
+                d = sqn.solve_one_step(self.f, self.g, X, None, k)
+                print d[1:10]
                 self.recon = []
                 if k > sqn.options['max_iter'] or sqn.termination_counter > 4:
                     iterations = k
@@ -178,12 +168,17 @@ class SqnDictionaryLearning(StochasticDictionaryLearning):
 
             if iterations < sqn.options['max_iter']:
                 print("Terminated successfully!")
-
-
             
             print "SQN done!"
 
-            self.components = D
-
 if __name__ == '__main__':
         print "main"
+        sd = SqnDictionaryLearning()
+        sd.n_components = 3
+        D = np.matrix([[1,2,3],[4,5,6]])
+        print D
+        print sd.matrix_to_vector(D)
+        print D.flat[:]
+        v = sd.matrix_to_vector(D)
+        print sd.vector_to_matrix(v)
+        print v.reshape(2, len(v)/2)
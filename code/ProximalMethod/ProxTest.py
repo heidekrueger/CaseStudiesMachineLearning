925bb9b1b497f59017dc3933168ce78c6854f008
FinBauer <fin.bauer@gmail.com>
2015-07-05 15:38:00 +0200

    Prox

--- code/ProximalMethod/ProxTest.py
+++ code/ProximalMethod/ProxTest.py
@@ -26,12 +26,13 @@ def initialize_lasso(size_A, l):
     np.random.seed(0)
     A = np.random.normal(size = (m, n))
     b = np.random.normal(size = (m, 1))
-    b_l_bfgs_b = np.random.normal(size = m)
+    b_l_bfgs_b = b.copy()
+    b_l_bfgs_b = b_l_bfgs_b.reshape(m)
     A_sq = np.dot(A.T, A)
     Ab = np.dot(A.T, b)
     Ab_l_bfgs_b = np.concatenate((np.dot(A.T, b_l_bfgs_b),-np.dot(A.T,b_l_bfgs_b))).T
     x0 = np.ones((n, 1))
-    x0_l_bfgs_b = np.ones(2 * n)
+    x0_l_bfgs_b = np.concatenate((np.ones(n), np.zeros(n)))
     bounds = 2 * n * [(0, None)]
     L = sp.linalg.eigvals(A_sq).real.max()
     
@@ -48,12 +49,17 @@ def prox_comparison():
     fval_prox_grad = pg.proximal_gradient(f, gf, x0, 1 / L, l_reg = l)
     spopt.fmin_l_bfgs_b(f_l_bfgs_b, x0_l_bfgs_b, gf_l_bfgs_b, 
                         bounds = bounds, callback = read_fval)
-    
-    plt.plot(range(len(fval_0sr1)), fval_0sr1, 'r',
-             range(len(fval_prox_grad)), fval_prox_grad, 'b',
-             range(len(fval_l_bfgs_b)), fval_l_bfgs_b, 'g')
-    plt.ylim([0, 100])
-    plt.xlim([0, 1000])
+    fval_0sr1.insert(0, f(x0))
+    fval_prox_grad.insert(0, f(x0))
+    fval_l_bfgs_b.insert(0, f_l_bfgs_b(x0_l_bfgs_b))
+    plt.plot(range(len(fval_0sr1)), fval_0sr1, 'r', label = '0sr1',
+             range(len(fval_prox_grad)), fval_prox_grad, 'b', label = 'ProxGrad',
+             range(len(fval_l_bfgs_b)), fval_l_bfgs_b, 'g', label = 'LBFGSB', lw = 2)
+    plt.xlim([0, 60])
+    plt.yscale('log')
+    plt.ylabel('Function Value')
+    plt.xlabel('Number of Iterations')
+    plt.legend()
 
     return
 
@@ -62,7 +68,7 @@ if __name__ == "__main__":
     import numpy as np
     import scipy as sp
     
-    A, b, b_l_bfgs_b, A_sq, Ab, Ab_l_bfgs_b, x0, x0_l_bfgs_b, bounds, l, L = initialize_lasso((15, 30), 1)
+    A, b, b_l_bfgs_b, A_sq, Ab, Ab_l_bfgs_b, x0, x0_l_bfgs_b, bounds, l, L = initialize_lasso((15, 30), 2)
     fval_l_bfgs_b = []
     
     def f(x):
@@ -81,8 +87,8 @@ if __name__ == "__main__":
     def gf_l_bfgs_b(x):
         n = len(x)
         temp = np.dot(A_sq, x[:np.floor(n/2)]) - np.dot(A_sq, x[np.floor(n/2):])
-        return np.concatenate((temp, -temp)).T - Ab_l_bfgs_b + l * np.ones(n)
-        
+        return np.concatenate((temp, -temp)).T - Ab_l_bfgs_b + l
+    
     prox_comparison()
     
     #test_gamma()

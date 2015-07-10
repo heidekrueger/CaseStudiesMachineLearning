ad33ed83b696796936c105be51956a958475df5e
Slasnista <stan.chambon@gmail.com>
2015-07-10 21:30:53 +0200

    sqn on eeg performs well

--- code/eeg_testing_sqn.py
+++ code/eeg_testing_sqn.py
@@ -5,14 +5,14 @@ Created on Tuesday 9 July
 @author: Roland Halbich / Stanislas Chambon
 """
 
-# from ProxEegTest import prox_meth_lr, predict
 import numpy as np
 from scipy import stats
-
 from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.metrics import roc_auc_score
 from SQN.SGD import SQN
 from SQN.LogisticRegression import LogisticRegression
+from time import time
+
 
 # load datasets
 X = np.load('../datasets/eeg_data.npy')
@@ -29,8 +29,11 @@ X /= nn[:, None]
 XX = np.concatenate((np.std(X, axis=1)[:, None],
                      stats.kurtosis(X, axis=1)[:, None]), axis=1)
 
+# number of runs for cv
+n_cv = 5
+
 # creation of a cross-validation object
-sss = StratifiedShuffleSplit(y, 5, train_size=0.8, random_state=0)
+sss = StratifiedShuffleSplit(y, n_cv, train_size=0.8, random_state=0)
 
 # parameter to modify
 w0 = np.array(XX[0].flat)
@@ -49,6 +52,9 @@ options = {'dim': len(X[0]),
            'testinterval': 100,
            'w1': w0}
 
+# time counting
+t0 = time()
+
 # cross-validation loop
 for train, test in sss:
     sqn = SQN(options)
@@ -61,11 +67,18 @@ for train, test in sss:
     y_test = [y[i] for i in test]
     logreg.w = sqn.solve(logreg.F, logreg.G, x_train, y_train)
 
-    y_pred = logreg.predict(x_test)
+    y_pred = 1 - logreg.predict(x_test)
 
     auc = roc_auc_score(y_test, y_pred)
     scores.append(auc)
 
+# compute running time
+dt = (time() - t0) / float(n_cv)
+
+
 # print scores
 scores = np.asmatrix(scores)
-print scores
+
+print "mean score    :", scores.mean()
+print "std, score    :", scores.std()
+print "running time  :", dt

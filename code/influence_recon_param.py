9b1f6bcff0bf05e70e2f7ea9895e668e72b69e44
Slasnista <stan.chambon@gmail.com>
2015-07-10 15:36:19 +0200

    creation of a function to reconstruct photos in parallel

--- code/influence_recon_param.py
+++ code/influence_recon_param.py
@@ -11,8 +11,79 @@ import numpy as np
 from time import time
 import matplotlib.pyplot as plt
 
+from sqndict import SqnDictionaryLearning
+from joblib import Parallel, delayed
 
-def influence(D, algo='omp', n_nonzero_coefs=None):
+
+def inner_rec(lena, data, intercept, d, algo='omp', nzc=None):
+    '''
+    INPUTS:
+
+    OUTPUTS:
+    - reconstructed photographs
+    '''
+    rec = lena.copy()
+
+    # encode noisy patches with learnt dictionary
+    code = sparse_encode(data,
+                         d.T, gram=None,
+                         cov=None,
+                         algorithm=algo,
+                         n_nonzero_coefs=nzc,
+                         alpha=None,
+                         copy_cov=True,
+                         init=None,
+                         max_iter=1000,
+                         n_jobs=1)
+
+    patch_size = (7, 7)
+    height, width = lena.shape
+
+    patches = np.dot(code, d.T)
+    patches += intercept
+    patches = patches.reshape(len(data), *patch_size)
+
+    # reconstruct noisy image
+    rec[:, height // 2:] = reconstruct_from_patches_2d(patches,
+                                                       (width, height // 2))
+
+    return rec
+
+
+def outer_reconstruction(influence, lena, algo='omp', nzc=None, n_jobs=1):
+    '''
+    INPUTS:
+    - influence : l_params, l_dict
+    - lena
+    '''
+
+    data, lena, distorted = preprocess_data(lena)
+
+    # post process data
+    data, intercept = postprocess_data(lena)
+
+    l_params = influence[0]
+    l_dict = influence[1]
+
+    rec = Parallel(n_jobs=n_jobs)(delayed(inner_rec)(lena,
+                                                     data,
+                                                     intercept,
+                                                     d,
+                                                     algo=algo,
+                                                     nzc=nzc)
+                                  for d in l_dict)
+    print len(rec)
+
+    # show the difference
+    for r in rec:
+        show_with_diff(r, lena, '')
+
+    # t0 = time()
+    # title = algo + 'with ' + str(n_nonzero_coefs) + ' n atoms'
+    # reconstructions = lena.copy()
+
+
+def influences(D, algo='omp', n_nonzero_coefs=None):
     '''
     reconstructs the distorted image
 
@@ -65,20 +136,29 @@ def influence(D, algo='omp', n_nonzero_coefs=None):
                    title + ' (time: %.1fs)' % dt)
 
 if __name__ == '__main__':
+    # loads lena
+    from scipy.misc import lena
 
-    # loads dictionary
-    D = np.load('dictionary.npy')
+    # loads dictionaries
+    influence = np.load('influence_n_iter.npy')
 
-    # plots dictionary for fun
-    plot_dictionary(D)
+    outer_reconstruction(influence, lena, algo='lars', nzc=10, n_jobs=5)
 
-    # creates a list of reconstruction algo and parameters
-    l_algos = []
-    for i in range(1, 10):
-        l_algos.append({'algo': 'lars',
-                        'n_nonzero_coefs': i})
-
-    # performs reconstruction for each set of parameters
-    for a in l_algos:
-        influence(D, algo=a['algo'], n_nonzero_coefs=a['n_nonzero_coefs'])
+    # plot dictionaries
     plt.show()
+
+    # # loads dictionary
+    # D = np.load('dictionary.npy')
+
+    # # plots dictionary for fun
+    # plot_dictionary(D)
+
+    # # creates a list of reconstruction algo and parameters
+    # l_algos = []
+    # for i in range(1, 10):
+    #     l_algos.append({'algo': 'lars',
+    #                     'n_nonzero_coefs': i})
+
+    # # performs reconstruction for each set of parameters
+    # for a in l_algos:
+    #     influence(D, algo=a['algo'], n_nonzero_coefs=a['n_nonzero_coefs'])

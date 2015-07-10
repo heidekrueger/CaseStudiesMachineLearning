b88c3d702f70fcf5d105f14bcc7a68c950bbaa10
Slasnista <stan.chambon@gmail.com>
2015-07-10 15:57:28 +0200

    modified influence_recon_param

--- code/influence_recon_param.py
+++ code/influence_recon_param.py
@@ -18,10 +18,17 @@ from joblib import Parallel, delayed
 def inner_rec(lena, data, intercept, d, algo='omp', nzc=None):
     '''
     INPUTS:
+    - lena
+    - data
+    - intercept
+    - d : dictionary
+    - algo : reconstruction algorithm to use
+    - nzc : number of non zero coefficients
 
     OUTPUTS:
-    - reconstructed photographs
+    - reconstructed photograph
     '''
+    # copy the photo
     rec = lena.copy()
 
     # encode noisy patches with learnt dictionary
@@ -36,9 +43,11 @@ def inner_rec(lena, data, intercept, d, algo='omp', nzc=None):
                          max_iter=1000,
                          n_jobs=1)
 
+    # dimension determination
     patch_size = (7, 7)
     height, width = lena.shape
 
+    # patch reconstruction
     patches = np.dot(code, d.T)
     patches += intercept
     patches = patches.reshape(len(data), *patch_size)
@@ -47,16 +56,26 @@ def inner_rec(lena, data, intercept, d, algo='omp', nzc=None):
     rec[:, height // 2:] = reconstruct_from_patches_2d(patches,
                                                        (width, height // 2))
 
+    # return reconstructed image
     return rec
 
 
-def outer_reconstruction(influence, lena, algo='omp', nzc=None, n_jobs=1):
+def outer_reconstruction(influence, lena,
+                         pi='n_components', algo='omp', nzc=None, n_jobs=1):
     '''
     INPUTS:
     - influence : l_params, l_dict
-    - lena
+    - lena : photograph to reconstruct
+    - pi : parameters which is investigated
+    - algo : reconstruction algo to use
+    - nzc : number of non zero coefficients
+    - n_jobs : number of jobs to run in parallel
+
+    OUTPUTS:
+    - Plot reconstructed photographs
     '''
 
+    # data preprocessing
     data, lena, distorted = preprocess_data(lena)
 
     # post process data
@@ -65,6 +84,7 @@ def outer_reconstruction(influence, lena, algo='omp', nzc=None, n_jobs=1):
     l_params = influence[0]
     l_dict = influence[1]
 
+    # parallel loop for parallelization
     rec = Parallel(n_jobs=n_jobs)(delayed(inner_rec)(lena,
                                                      data,
                                                      intercept,
@@ -74,66 +94,13 @@ def outer_reconstruction(influence, lena, algo='omp', nzc=None, n_jobs=1):
                                   for d in l_dict)
     print len(rec)
 
-    # show the difference
-    for r in rec:
-        show_with_diff(r, lena, '')
+    # plot reconstructed images
+    for i in range(0, len(rec)):
+        p = l_params[i]
+        show_with_diff(rec[i], lena, pi + ' : ' + str(p[pi]))
 
-    # t0 = time()
-    # title = algo + 'with ' + str(n_nonzero_coefs) + ' n atoms'
-    # reconstructions = lena.copy()
-
-
-def influences(D, algo='omp', n_nonzero_coefs=None):
-    '''
-    reconstructs the distorted image
-
-    INPUTS:
-    - D : dictionary to use
-    - algo : reconstruction algorithm : 'omp', 'lars'
-    - n_nonzero_coefs : int, necessary to control the accuracy of the
-    reconstruction
-
-    OUTPUS:
-    - picture of the distorted image
-    - picture of the reconsted image
-    '''
-
-    from scipy.misc import lena
-
-    data, lena, distorted = preprocess_data(lena)
-
-    # post process data
-    data, intercept = postprocess_data(lena)
-
-    t0 = time()
-    title = algo + 'with ' + str(n_nonzero_coefs) + ' n atoms'
-    reconstructions = lena.copy()
-
-    # encode noisy patches with learnt dictionary
-    code = sparse_encode(data, D.T, gram=None,
-                         cov=None, algorithm=algo,
-                         n_nonzero_coefs=n_nonzero_coefs, alpha=None,
-                         copy_cov=True, init=None,
-                         max_iter=1000, n_jobs=1)
-
-    patch_size = (7, 7)
-    height, width = lena.shape
-
-    patches = np.dot(code, D.T)
-    patches += intercept
-    patches = patches.reshape(len(data), *patch_size)
-
-    # reconstruct noisy image
-    reconstructions[:, height // 2:] = reconstruct_from_patches_2d(
-        patches, (width, height // 2))
-
-    dt = time() - t0
-    print('done in %.2fs.' % dt)
-
-    # show the difference
+    # plot distorded image
     show_with_diff(distorted, lena, 'Distorted image')
-    show_with_diff(reconstructions, lena,
-                   title + ' (time: %.1fs)' % dt)
 
 if __name__ == '__main__':
     # loads lena
@@ -142,23 +109,12 @@ if __name__ == '__main__':
     # loads dictionaries
     influence = np.load('influence_n_iter.npy')
 
-    outer_reconstruction(influence, lena, algo='lars', nzc=10, n_jobs=5)
+    outer_reconstruction(influence,
+                         lena,
+                         pi='n_iter',
+                         algo='lars',
+                         nzc=10,
+                         n_jobs=5)
 
     # plot dictionaries
     plt.show()
-
-    # # loads dictionary
-    # D = np.load('dictionary.npy')
-
-    # # plots dictionary for fun
-    # plot_dictionary(D)
-
-    # # creates a list of reconstruction algo and parameters
-    # l_algos = []
-    # for i in range(1, 10):
-    #     l_algos.append({'algo': 'lars',
-    #                     'n_nonzero_coefs': i})
-
-    # # performs reconstruction for each set of parameters
-    # for a in l_algos:
-    #     influence(D, algo=a['algo'], n_nonzero_coefs=a['n_nonzero_coefs'])
